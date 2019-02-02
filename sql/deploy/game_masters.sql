231afaa1aab445d8c9fa8bd528c01de7d28974da
Denis Fedoseev <denis.fedoseev@gmail.com>
2019-02-03 01:21:33 +0300

    fk to games

--- sql/deploy/game_masters.sql
+++ sql/deploy/game_masters.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
-CREATE TABLE `game_masters` ( `user_id` INTEGER NOT NULL, `game_id` INTEGER NOT NULL, FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) );
+CREATE TABLE `game_masters` (
+    `user_id` INTEGER NOT NULL,
+    `game_id` INTEGER NOT NULL,
+    FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY(`game_id`) REFERENCES `games`(`game_id`)
+);
 
 COMMIT;
